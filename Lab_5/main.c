aa965d04094400bed72338894bea7a171e38627d
sekena1 <sekena1@mcmaster.ca>
2025-12-01 21:11:23 -0500

    Initial Commit

---
+++ Lab_5/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Lab_5
+//
+//  Created by Arnur Seken on 2025-12-01.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
